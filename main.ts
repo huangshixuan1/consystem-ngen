4f1873744deba452cb5677731c7655e00f66cfae
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-03-06 19:32:36 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,36 @@
+function Hua_Close () {
+    pins.servoWritePin(AnalogPin.P1, 0)
+}
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         平穩1()
-        basic.pause(100)
+        basic.pause(50)
         STOP()
     } else if (receivedNumber == 2) {
         L開2()
-        basic.pause(100)
+        basic.pause(50)
         STOP()
     } else if (receivedNumber == 3) {
         Jia_Open()
         basic.pause(100)
-        STOP()
     } else if (receivedNumber == 4) {
         Jia_Close()
         basic.pause(100)
+    } else if (receivedNumber == 5) {
+        basic.pause(100)
+        Hua_Open()
+    } else if (receivedNumber == 6) {
+        basic.pause(100)
+        Hua_Close()
+    } else {
         STOP()
     }
 })
 function Jia_Open () {
-    pins.servoWritePin(AnalogPin.P0, 180)
+    pins.servoWritePin(AnalogPin.P1, 180)
 }
 function Jia_Close () {
-    pins.servoWritePin(AnalogPin.P0, 0)
+    pins.servoWritePin(AnalogPin.P1, 0)
 }
 function STOP () {
     sensors.DDMmotor(
@@ -30,7 +39,8 @@ function STOP () {
     AnalogPin.P14,
     0
     )
-    pins.servoWritePin(AnalogPin.P0, 0)
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P8, 90)
 }
 function L開2 () {
     sensors.DDMmotor(
@@ -48,6 +58,10 @@ function 平穩1 () {
     200
     )
 }
+function Hua_Open () {
+    pins.servoWritePin(AnalogPin.P1, 180)
+}
+STOP()
 radio.setGroup(174)
 basic.forever(function () {
 	
