f9c5b6e9579b90c4409c25ac4309fc53ff260f9c
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-03-06 12:37:11 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == "1") {
+    if (receivedNumber == 1) {
         平穩1()
-    } else if (receivedNumber == "2") {
+    } else if (receivedNumber == 2) {
         L開2()
     }
 })
 function L開2 () {
     sensors.DDMmotor(
     AnalogPin.P0,
-    0,
+    1,
     AnalogPin.P0,
     0
     )
