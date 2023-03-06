4b404268d2ddfa1c779ee394b8822ebb4ed7aedd
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-03-06 17:52:22 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         平穩1()
+        basic.pause(100)
+        STOP()
     } else if (receivedNumber == 2) {
         L開2()
+        basic.pause(100)
+        STOP()
     }
 })
+function STOP () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    0
+    )
+}
 function L開2 () {
     sensors.DDMmotor(
     AnalogPin.P13,
