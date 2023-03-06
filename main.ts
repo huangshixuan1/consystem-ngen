faba8cfa73ef7b62ed3b5d34e7d0e3f3c78f0a83
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-03-06 20:58:11 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,51 +1,41 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        平穩1()
+        讓左右跑的夾子的軌道回家()
         basic.pause(50)
-        Al_STOP()
+        讓左右跑的那個夾子軌道停止()
     } else if (receivedNumber == 2) {
-        L開2()
+        讓左右跑的夾子的軌道出門()
         basic.pause(50)
-        Al_STOP()
+        讓左右跑的那個夾子軌道停止()
     } else if (receivedNumber == 3) {
-        Jia_Open()
+        伺服馬達P1夾子開()
         basic.pause(100)
     } else if (receivedNumber == 4) {
-        Jia_Close()
+        伺服馬達P1夾子關()
         basic.pause(100)
     } else if (receivedNumber == 5) {
-        Hua_Kai()
+        整個軌道旋轉逆時鐘()
         basic.pause(200)
-        Al_STOP()
+        伺服馬達全部停止()
     } else if (receivedNumber == 6) {
-        Hua_Guan()
+        整個軌道旋轉順時鐘()
         basic.pause(200)
-        Al_STOP()
+        伺服馬達全部停止()
     } else if (receivedNumber == 7) {
-        Qian7()
-        basic.pause(100)
-        Al_STOP()
+    	
     } else if (receivedNumber == 8) {
-        Hoo8()
-        basic.pause(100)
-        Al_STOP()
+    	
     } else {
-        Al_STOP()
+        讓左右跑的那個夾子軌道停止()
     }
 })
-function Jia_Open () {
-    pins.servoWritePin(AnalogPin.P1, 180)
-}
-function Jia_Close () {
-    pins.servoWritePin(AnalogPin.P1, 0)
-}
-function Qian7 () {
-	
-}
-function Hua_Kai () {
+function 整個軌道旋轉逆時鐘 () {
     pins.servoWritePin(AnalogPin.P8, 180)
 }
-function L開2 () {
+function 整個軌道旋轉順時鐘 () {
+    pins.servoWritePin(AnalogPin.P8, 0)
+}
+function 讓左右跑的夾子的軌道出門 () {
     sensors.DDMmotor(
     AnalogPin.P13,
     1,
@@ -59,7 +49,24 @@ function L開2 () {
     200
     )
 }
-function Al_STOP () {
+function 伺服馬達P1夾子開 () {
+    pins.servoWritePin(AnalogPin.P1, 180)
+}
+function 讓左右跑的夾子的軌道回家 () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    200
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    200
+    )
+}
+function 讓左右跑的那個夾子軌道停止 () {
     sensors.DDMmotor(
     AnalogPin.P13,
     0,
@@ -72,31 +79,13 @@ function Al_STOP () {
     AnalogPin.P16,
     0
     )
+}
+function 伺服馬達P1夾子關 () {
+    pins.servoWritePin(AnalogPin.P1, 0)
+}
+function 伺服馬達全部停止 () {
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P8, 90)
 }
-function Hoo8 () {
-	
-}
-function Hua_Guan () {
-    pins.servoWritePin(AnalogPin.P8, 0)
-}
-function 平穩1 () {
-    sensors.DDMmotor(
-    AnalogPin.P13,
-    0,
-    AnalogPin.P14,
-    200
-    )
-    sensors.DDMmotor(
-    AnalogPin.P15,
-    0,
-    AnalogPin.P16,
-    200
-    )
-}
-Al_STOP()
+讓左右跑的那個夾子軌道停止()
 radio.setGroup(174)
-basic.forever(function () {
-	
-})
