11c264fd7e7dbb245c82ee2f09f436e5d7381cc1
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-03-06 19:00:14 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,8 +7,22 @@ radio.onReceivedNumber(function (receivedNumber) {
         L開2()
         basic.pause(100)
         STOP()
+    } else if (receivedNumber == 3) {
+        Jia_Open()
+        basic.pause(100)
+        STOP()
+    } else if (receivedNumber == 4) {
+        Jia_Close()
+        basic.pause(100)
+        STOP()
     }
 })
+function Jia_Open () {
+    pins.servoWritePin(AnalogPin.P0, 180)
+}
+function Jia_Close () {
+    pins.servoWritePin(AnalogPin.P0, 0)
+}
 function STOP () {
     sensors.DDMmotor(
     AnalogPin.P13,
@@ -16,6 +30,7 @@ function STOP () {
     AnalogPin.P14,
     0
     )
+    pins.servoWritePin(AnalogPin.P0, 0)
 }
 function L開2 () {
     sensors.DDMmotor(
