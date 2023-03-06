ef3359a0f7be617bd22c4a08df789d9f837fcfc6
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-03-06 20:43:09 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
-function Hua_Close () {
-    pins.servoWritePin(AnalogPin.P8, 0)
-}
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         平穩1()
         basic.pause(50)
-        STOP()
+        Al_STOP()
     } else if (receivedNumber == 2) {
         L開2()
         basic.pause(50)
-        STOP()
+        Al_STOP()
     } else if (receivedNumber == 3) {
         Jia_Open()
         basic.pause(100)
@@ -17,13 +14,23 @@ radio.onReceivedNumber(function (receivedNumber) {
         Jia_Close()
         basic.pause(100)
     } else if (receivedNumber == 5) {
-        Hua_Open()
+        Hua_Kai()
         basic.pause(200)
+        Al_STOP()
     } else if (receivedNumber == 6) {
-        Hua_Close()
+        Hua_Guan()
         basic.pause(200)
+        Al_STOP()
+    } else if (receivedNumber == 7) {
+        Qian7()
+        basic.pause(100)
+        Al_STOP()
+    } else if (receivedNumber == 8) {
+        Hoo8()
+        basic.pause(100)
+        Al_STOP()
     } else {
-        STOP()
+        Al_STOP()
     }
 })
 function Jia_Open () {
@@ -32,15 +39,11 @@ function Jia_Open () {
 function Jia_Close () {
     pins.servoWritePin(AnalogPin.P1, 0)
 }
-function STOP () {
-    sensors.DDMmotor(
-    AnalogPin.P13,
-    0,
-    AnalogPin.P14,
-    0
-    )
-    pins.servoWritePin(AnalogPin.P1, 0)
-    pins.servoWritePin(AnalogPin.P8, 90)
+function Qian7 () {
+	
+}
+function Hua_Kai () {
+    pins.servoWritePin(AnalogPin.P8, 180)
 }
 function L開2 () {
     sensors.DDMmotor(
@@ -49,6 +52,34 @@ function L開2 () {
     AnalogPin.P14,
     200
     )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    100
+    )
+}
+function Al_STOP () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    0
+    )
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P8, 90)
+}
+function Hoo8 () {
+	
+}
+function Hua_Guan () {
+    pins.servoWritePin(AnalogPin.P8, 0)
 }
 function 平穩1 () {
     sensors.DDMmotor(
@@ -57,11 +88,14 @@ function 平穩1 () {
     AnalogPin.P14,
     200
     )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    1,
+    AnalogPin.P16,
+    100
+    )
 }
-function Hua_Open () {
-    pins.servoWritePin(AnalogPin.P8, 180)
-}
-STOP()
+Al_STOP()
 radio.setGroup(174)
 basic.forever(function () {
 	
